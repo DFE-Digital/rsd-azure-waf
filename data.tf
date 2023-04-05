f9afd9fd0409542ec266cd547a2e779e7de204ed
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-04-05 10:49:48 +0100

    Removed data

--- data.tf
+++
@@ -1,7 +0,0 @@
-data "azurerm_cdn_frontdoor_endpoint" "origins" {
-  for_each = local.front_door_endpoints
-
-  name                = each.value.name
-  profile_name        = each.value.profile_name
-  resource_group_name = each.value.resource_group_name
-}
